1080ae5e222bbd7accda74a194d834a410530026
bhavyab212 <bhavyab212@users.noreply.github.com>
2026-02-26 13:20:06 +0530

    feat: setup React application foundation

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from './components/ui/Toast';
+import { useThemeStore } from './stores/themeStore';
+import LoginPage from './app/login/LoginPage';
+import RegisterPage from './app/register/RegisterPage';
+import OnboardingPage from './app/onboarding/OnboardingPage';
+
+function ThemeSync() {
+    const { theme } = useThemeStore();
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    return null;
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <ThemeSync />
+            <Toaster>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/onboarding" element={<OnboardingPage />} />
+                    <Route path="/" element={<Navigate to="/login" replace />} />
+                    <Route path="*" element={<Navigate to="/login" replace />} />
+                </Routes>
+            </Toaster>
+        </BrowserRouter>
+    );
+}
+
+export default App;
